e3cf9ece727618080539736b6792f3eb5ae1d295
Umberto Lentini <umberto.lentini@gmail.com>
2020-03-20 18:29:18 +0100

    Bump dependencies for security reasons

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,7 +4,15 @@ import { PasswordGenerator } from "./password-generator"
 
 const App: FC<{}> = () => {
   return (
-    <div className="bg-gray-100 h-screen flex items-center justify-center">
+    <div className="bg-gray-100 font-sans h-screen flex flex-col items-center justify-center">
+      <div className="text-center">
+        <h1 className="-mt-24 text-2xl font-semibold text-blue-800 p-0 pb-2 m-0 leading-tight">
+          PassGen
+        </h1>
+        <h2 className="font-light text-lg text-blue-700 p-0 m-0  leading-tight">
+          Password generator
+        </h2>
+      </div>
       <PasswordGenerator />
     </div>
   )
